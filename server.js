801cf4060d2744d79737605a53e47846d580c21a
ndt-dinhtu <ndt.290303060703@gmail.com>
2025-01-16 09:17:26 +0700

    open foder

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const categoryRoutes = require("./routes/categoryRoutes");
 const imageRoutes = require("./routes/imageRoutes");
 const cors = require("cors");
+const { exec } = require("child_process");
 dotenv.config();
 const app = express();
 
@@ -24,6 +25,22 @@ app.use(bodyParser.json());
 
 app.use("/api/categories", categoryRoutes);
 app.use("/api/images", imageRoutes);
+app.post("/open-folder", (req, res) => {
+  const { folderPath } = req.body;
+  if (!folderPath) {
+    return res.status(400).send("Đường dẫn thư mục không hợp lệ");
+  }
+
+  const correctedPath = folderPath.replace(/\\/g, "/");
+
+  exec(`start "" "${correctedPath}"`, (err) => {
+    if (err) {
+      res.status(500).send("Không thể mở thư mục");
+    } else {
+      res.send("Đã mở thư mục");
+    }
+  });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, "0.0.0.0", () => {
